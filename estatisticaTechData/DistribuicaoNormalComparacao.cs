8436b12d28f4f6dcc28988f58a6fcc913ce85a67
Elias <pinheirinho2004@gmail.com>
2022-11-07 22:55:04 -0300

    Tela de Comparação

---
+++ estatisticaTechData/DistribuicaoNormalComparacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estatisticaTechData
+{
+    public partial class DistribuicaoNormalComparacao : Form
+    {
+        public DistribuicaoNormalComparacao()
+        {
+            InitializeComponent();
+        }
+    }
+}
