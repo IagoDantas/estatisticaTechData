c126ae295cf6090943e8736a073b938a8aceea61
Iago Dantas <iagodantas.8@gmail.com>
2023-11-06 02:37:22 -0300

    feat: add file listing to file history tab

--- estatisticaTechData/Screens/UC_HistoricoArquivos.Designer.cs
+++ estatisticaTechData/Screens/UC_HistoricoArquivos.Designer.cs
@@ -30,18 +30,11 @@
         {
             this.lblNumeroPagina = new System.Windows.Forms.Label();
             this.pnlArquivos = new System.Windows.Forms.Panel();
-            this.pnlNome = new System.Windows.Forms.Panel();
-            this.TxtData = new System.Windows.Forms.TextBox();
-            this.TxtNome = new System.Windows.Forms.TextBox();
             this.lblDataArquivo = new System.Windows.Forms.Label();
             this.lblNomeArquivo = new System.Windows.Forms.Label();
             this.lblRecentes = new System.Windows.Forms.Label();
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.btnShow = new estatisticaTechData.techDataButton();
-            this.btnDelete = new estatisticaTechData.techDataButton();
-            this.btnRead = new estatisticaTechData.techDataButton();
             this.pnlArquivos.SuspendLayout();
-            this.pnlNome.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblNumeroPagina
@@ -58,11 +51,7 @@
             // 
             // pnlArquivos
             // 
-            this.pnlArquivos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlArquivos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(224)))), ((int)(((byte)(206)))));
-            this.pnlArquivos.Controls.Add(this.pnlNome);
             this.pnlArquivos.Controls.Add(this.lblDataArquivo);
             this.pnlArquivos.Controls.Add(this.lblNomeArquivo);
             this.pnlArquivos.Location = new System.Drawing.Point(99, 182);
@@ -71,48 +60,11 @@
             this.pnlArquivos.Size = new System.Drawing.Size(804, 430);
             this.pnlArquivos.TabIndex = 15;
             // 
-            // pnlNome
-            // 
-            this.pnlNome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlNome.Controls.Add(this.TxtData);
-            this.pnlNome.Controls.Add(this.btnShow);
-            this.pnlNome.Controls.Add(this.btnDelete);
-            this.pnlNome.Controls.Add(this.btnRead);
-            this.pnlNome.Controls.Add(this.TxtNome);
-            this.pnlNome.Location = new System.Drawing.Point(0, 64);
-            this.pnlNome.Name = "pnlNome";
-            this.pnlNome.Size = new System.Drawing.Size(801, 50);
-            this.pnlNome.TabIndex = 16;
-            // 
-            // TxtData
-            // 
-            this.TxtData.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.TxtData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(224)))), ((int)(((byte)(206)))));
-            this.TxtData.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.TxtData.Enabled = false;
-            this.TxtData.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtData.Location = new System.Drawing.Point(255, 8);
-            this.TxtData.Name = "TxtData";
-            this.TxtData.Size = new System.Drawing.Size(193, 29);
-            this.TxtData.TabIndex = 7;
-            // 
-            // TxtNome
-            // 
-            this.TxtNome.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.TxtNome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(224)))), ((int)(((byte)(206)))));
-            this.TxtNome.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.TxtNome.Enabled = false;
-            this.TxtNome.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtNome.Location = new System.Drawing.Point(8, 8);
-            this.TxtNome.Name = "TxtNome";
-            this.TxtNome.Size = new System.Drawing.Size(241, 29);
-            this.TxtNome.TabIndex = 1;
-            // 
             // lblDataArquivo
             // 
             this.lblDataArquivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblDataArquivo.AutoSize = true;
+            this.lblDataArquivo.Enabled = false;
             this.lblDataArquivo.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDataArquivo.Location = new System.Drawing.Point(604, 6);
             this.lblDataArquivo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -120,10 +72,12 @@
             this.lblDataArquivo.Size = new System.Drawing.Size(68, 34);
             this.lblDataArquivo.TabIndex = 1;
             this.lblDataArquivo.Text = "Data:";
+            this.lblDataArquivo.Visible = false;
             // 
             // lblNomeArquivo
             // 
             this.lblNomeArquivo.AutoSize = true;
+            this.lblNomeArquivo.Enabled = false;
             this.lblNomeArquivo.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNomeArquivo.Location = new System.Drawing.Point(32, 6);
             this.lblNomeArquivo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
@@ -131,6 +85,7 @@
             this.lblNomeArquivo.Size = new System.Drawing.Size(78, 34);
             this.lblNomeArquivo.TabIndex = 0;
             this.lblNomeArquivo.Text = "Nome:";
+            this.lblNomeArquivo.Visible = false;
             // 
             // lblRecentes
             // 
@@ -156,70 +111,6 @@
             this.lblTitulo.TabIndex = 19;
             this.lblTitulo.Text = "Histórico de arquivos";
             // 
-            // btnShow
-            // 
-            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnShow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(107)))), ((int)(((byte)(117)))));
-            this.btnShow.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(107)))), ((int)(((byte)(117)))));
-            this.btnShow.BorderColor = System.Drawing.Color.PapayaWhip;
-            this.btnShow.BorderRadius = 40;
-            this.btnShow.BorderSize = 0;
-            this.btnShow.FlatAppearance.BorderSize = 0;
-            this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnShow.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShow.ForeColor = System.Drawing.Color.White;
-            this.btnShow.Location = new System.Drawing.Point(581, 3);
-            this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(105, 40);
-            this.btnShow.TabIndex = 6;
-            this.btnShow.Text = "...";
-            this.btnShow.TextColor = System.Drawing.Color.White;
-            this.btnShow.UseVisualStyleBackColor = false;
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDelete.BackColor = System.Drawing.Color.Red;
-            this.btnDelete.BackgroundColor = System.Drawing.Color.Red;
-            this.btnDelete.BorderColor = System.Drawing.Color.PapayaWhip;
-            this.btnDelete.BorderRadius = 40;
-            this.btnDelete.BorderSize = 0;
-            this.btnDelete.Enabled = false;
-            this.btnDelete.FlatAppearance.BorderSize = 0;
-            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDelete.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.ForeColor = System.Drawing.Color.White;
-            this.btnDelete.Location = new System.Drawing.Point(692, 5);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(105, 40);
-            this.btnDelete.TabIndex = 5;
-            this.btnDelete.Text = "Deletar";
-            this.btnDelete.TextColor = System.Drawing.Color.White;
-            this.btnDelete.UseVisualStyleBackColor = false;
-            this.btnDelete.Visible = false;
-            // 
-            // btnRead
-            // 
-            this.btnRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(107)))), ((int)(((byte)(117)))));
-            this.btnRead.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(107)))), ((int)(((byte)(117)))));
-            this.btnRead.BorderColor = System.Drawing.Color.PapayaWhip;
-            this.btnRead.BorderRadius = 40;
-            this.btnRead.BorderSize = 0;
-            this.btnRead.Enabled = false;
-            this.btnRead.FlatAppearance.BorderSize = 0;
-            this.btnRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRead.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRead.ForeColor = System.Drawing.Color.White;
-            this.btnRead.Location = new System.Drawing.Point(470, 3);
-            this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(105, 40);
-            this.btnRead.TabIndex = 3;
-            this.btnRead.Text = "Visualizar";
-            this.btnRead.TextColor = System.Drawing.Color.White;
-            this.btnRead.UseVisualStyleBackColor = false;
-            this.btnRead.Visible = false;
-            // 
             // UC_HistoricoArquivos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -234,8 +125,6 @@
             this.Load += new System.EventHandler(this.UC_HistoricoArquivos_Load);
             this.pnlArquivos.ResumeLayout(false);
             this.pnlArquivos.PerformLayout();
-            this.pnlNome.ResumeLayout(false);
-            this.pnlNome.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,12 +140,6 @@
         private System.Windows.Forms.Label lblNomeArquivo;
         private System.Windows.Forms.Label lblRecentes;
         private techDataButton btnNovaCarga;
-        private System.Windows.Forms.Panel pnlNome;
-        private techDataButton btnShow;
-        private techDataButton btnDelete;
-        private techDataButton btnRead;
-        private System.Windows.Forms.TextBox TxtNome;
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.TextBox TxtData;
     }
 }
--- estatisticaTechData/Screens/UC_HistoricoArquivos.cs
+++ estatisticaTechData/Screens/UC_HistoricoArquivos.cs
@@ -32,30 +32,60 @@ namespace estatisticaTechData
             email = userInfo["email"].ToString();
             senha = userInfo["senha"].ToString();
 
-            // Obter a data do arquivo
-            string[] chargeColumns = { "date", "data", "status", "user_id" }; // Inclua a coluna "user_id"
-            /*string chargeWhere = $"user_id = {userId}";*/ // Substitua "userId" pela ID da carga específica
+            // Obtenha os dados da tabela "charge"
+            string[] chargeColumns = { "date", "data", "status", "user_id" };
             List<string>[] chargeResult = conexao.SelectData("charge", chargeColumns, "");
 
             if (chargeResult[0].Count > 0)
             {
-                // Obtenha a data da carga
-                fileDate = DateTime.Parse(chargeResult[0][0]);
-                TxtData.Text = fileDate.ToString();
+                int panelY = 2;
 
-                // Obtenha o user_id da carga
-                int cargaUserId = int.Parse(chargeResult[3][0].ToString());
-
-                // Agora, com o user_id da carga, você pode obter o nome do usuário da tabela "users"
-                string[] userColumns = { "name" };
-                string userWhere = $"id = {cargaUserId}";
-                List<string>[] userResult = conexao.SelectData("users", userColumns, userWhere);
-
-                if (userResult[0].Count > 0)
+                for (int i = 0; i < chargeResult[0].Count; i++)
                 {
-                    // Obtenha o nome do usuário
-                    string userName = userResult[0][0];
-                    TxtNome.Text = userName;
+                    // Obtenha a data da carga
+                    DateTime fileDate = DateTime.Parse(chargeResult[0][i]);
+                    string dateText = fileDate.ToString();
+
+                    // Obtenha o user_id da carga
+                    int cargaUserId = int.Parse(chargeResult[3][i].ToString());
+
+                    // Obtenha o nome do usuário da tabela "users"
+                    string[] userColumns = { "name" };
+                    string userWhere = $"id = {cargaUserId}";
+                    List<string>[] userResult = conexao.SelectData("users", userColumns, userWhere);
+
+                    if (userResult[0].Count > 0)
+                    {
+                        // Obtenha o nome do usuário
+                        string cargaUserName = userResult[0][0];
+
+                        // Crie um novo Panel com o estilo desejado
+                        Panel panel = new Panel();
+                        panel.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+                        panel.BorderStyle = BorderStyle.FixedSingle;
+                        panel.Location = new Point(10, panelY); // Ajuste as coordenadas conforme necessário
+                        panel.Size = new Size(801, 50);
+
+                        // Crie um Label para exibir o nome
+                        Label labelNome = new Label();
+                        labelNome.Text = $"Nome: {cargaUserName}";
+                        labelNome.Location = new Point(10, 10); // Ajuste as coordenadas conforme necessário
+
+                        // Crie um Label para exibir a data e ajuste sua posição para que fique ao lado do Label do nome
+                        Label labelData = new Label();
+                        labelData.Text = $"Data: {dateText}";
+                        labelData.Location = new Point(labelNome.Right + 10, 10); // Ajuste as coordenadas conforme necessário
+
+                        // Adicione os Labels ao Panel
+                        panel.Controls.Add(labelNome);
+                        panel.Controls.Add(labelData);
+                        
+
+                        // Adicione o Panel ao seu formulário
+                        this.Controls.Add(panel);
+                        pnlArquivos.Controls.Add(panel);
+                        panelY += panel.Height + 10;
+                    }
                 }
             }
         }
